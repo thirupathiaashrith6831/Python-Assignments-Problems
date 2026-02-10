b034294d9b7963b896263e97515ca39b66b956d2
thirupathiaashrith6831 <thirupathiaashrith6831@gmail.com>
2026-02-10 20:35:43 +0530

    Add files via upload

---
+++ set 6.py
@@ -0,0 +1,5 @@
+a = int(input('Enter input:'))
+
+print("It's square is :",a**2) #it is the command for powering any number
+
+print("It's cube is :",a**3) #it is the command for powering any number
