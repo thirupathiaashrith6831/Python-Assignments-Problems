958dd9e769c8d5832211df101b8dfa9334d3347d
thirupathiaashrith6831 <thirupathiaashrith6831@gmail.com>
2026-02-11 21:42:21 +0530

    Add files via upload

---
+++ str_function.py
@@ -0,0 +1,27 @@
+str =  "aashrith"
+print(len(str)) #output=5
+
+str1 = "aashrith"
+d = str1.endswith("rith")
+print(d)
+
+str2 = "aashrith"
+print(str2.startswith("aashr"))
+
+str3 = "aashrith"
+count = str3.count("h")
+print(count)
+
+str4 = "aashrith"
+b = str4.capitalize()
+print(b)
+
+str5 = "aashrith"
+Index = str5.find("h")
+print(Index)
+
+str6 = "Aashrith is good"
+g = str6.replace("good","great")
+print(g)
+
+
