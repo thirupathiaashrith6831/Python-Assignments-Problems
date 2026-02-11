5e07e3b179ee8536da4af8f2cbff508c2000eb42
thirupathiaashrith6831 <thirupathiaashrith6831@gmail.com>
2026-02-11 21:42:57 +0530

    Add files via upload

---
+++ 01_list.py
@@ -0,0 +1,15 @@
+friends = ["Apple","red",45,67.87,True,"Aashrith"]
+
+print(friends[1])
+
+print(friends[3])
+
+print(friends[4])
+
+#print(friends[6]) #error
+
+friends[1] = 34
+
+print(friends[1]) #lists are mutable unlike strings
+
+print(friends[0:4])
