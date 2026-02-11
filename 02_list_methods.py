667105775eaac5fe1c1ee089d23d45ede2f6cd33
thirupathiaashrith6831 <thirupathiaashrith6831@gmail.com>
2026-02-11 21:43:11 +0530

    Add files via upload

---
+++ 02_list_methods.py
@@ -0,0 +1,21 @@
+friends = ["Apple","red",45,67.87,True,"Aashrith"]
+print(friends)
+friends.append("White")
+print(friends)
+
+l1 = [1,34,56,22,15,45]
+l1.sort()
+print(l1)
+
+l1.reverse()
+print(l1)
+
+l1.insert(3,4)
+print(l1)
+
+l1.pop(3)
+print(l1)
+
+l1.remove(15)
+print(l1)
+
