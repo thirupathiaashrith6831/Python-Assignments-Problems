6a105334cc640c09af98b88deb1eceb62f06b159
thirupathiaashrith6831 <thirupathiaashrith6831@gmail.com>
2026-02-11 21:41:25 +0530

    Add files via upload

---
+++ Eascape_sequence characters.py
@@ -0,0 +1,7 @@
+print("I am very sad \n but i have to") # newline
+
+print("Ironman is a great Hero \t And a great mechanic") # tab
+
+print("Captain is a good person \' but also a great hero") # single quote
+
+print("Marvel studios is best \\ but they do average movies") # Backslash
