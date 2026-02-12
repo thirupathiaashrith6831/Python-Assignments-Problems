f0c7119e3e567469efcd12c02d6795ed5de8c2f1
thirupathiaashrith6831 <thirupathiaashrith6831@gmail.com>
2026-02-12 21:57:52 +0530

    Add files via upload

---
+++ 01_functions.py
@@ -0,0 +1,22 @@
+#a = int(input("enter the number:"))
+#b = int(input("enter the number:"))
+#c = int(input("enter the number:"))
+
+#average = (a+b+c)/3
+
+#fucntion defination
+def avg():
+    a = int(input("enter the number:"))
+    b = int(input("enter the number:"))
+    c = int(input("enter the number:"))
+
+    average = (a+b+c)/3
+    print(average)
+
+#function call
+avg()
+
+
+
+
+
